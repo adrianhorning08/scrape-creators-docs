cc97091ab7316d6d2cb5e5ff6db4c2418867bdcd
Adrian Horning <m.adrian.horning@gmail.com>
2025-03-27 21:14:57 -0500

    update

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -15192,7 +15192,8 @@ export const apis = [
       {
         name: "Company Ads",
         method: "GET",
-        description: "Get the ads for a company",
+        description:
+          "Scrape a company's ads. *This only gets the public ads. Some ads you need to log in for and sadly we can't get those. Also, since there are so many variations, the return types might not all be 100% the same. Email me for suggestions. Also this endpoint is pretty new, if you find any errors or irregularities, please email me: adrian@thewebscrapingguy.com",
         path: "/v1/google/company/ads",
         sampleResponse: {
           ads: [
