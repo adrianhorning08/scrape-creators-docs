e5200bbe02502dd594a1f9ea05c719f59e00350d
Adrian Horning <m.adrian.horning@gmail.com>
2025-04-10 16:21:45 -0500

    truth social

---
+++ src/TruthSocialIcon.jsx
@@ -0,0 +1,13 @@
+export const TruthSocialIcon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="20" cy="20" r="20" fill="currentColor" />
+    <path d="M13 12H27V15H22V28H18V15H13V12Z" fill="white" />
+    <rect x="25" y="26" width="2" height="2" fill="white" />
+  </svg>
+);
