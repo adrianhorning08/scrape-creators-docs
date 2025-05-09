522d20732d971b83b2fefe17ed7e21367395e0ed
Adrian Horning <m.adrian.horning@gmail.com>
2025-05-09 18:19:03 -0500

    pinterest search

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -19762,7 +19762,130 @@ export const apis = [
     name: "Pinterest",
     icon: SiPinterest,
     description: "Scrape Pinterest pins",
+    responseFields: [
+      {
+        path: "pins[0].story_pin_data.pages_preview[0].blocks[0].video.video_list.V_EXP7",
+        description: "Video URL",
+      },
+    ],
     endpoints: [
+      {
+        name: "Search",
+        method: "GET",
+        description: "Search Pinterest",
+        path: "/v1/pinterest/search",
+        params: [
+          {
+            name: "query",
+            type: "string",
+            required: true,
+            description: "Search query",
+            placeholder: "Italian Pot Roast",
+          },
+          {
+            name: "cursor",
+            type: "string",
+            required: false,
+            description: "Cursor",
+            placeholder: "Y2JVSG81V2sxcmNHRlpWM1J...",
+          },
+        ],
+        sampleResponse: {
+          success: true,
+          pins: [
+            {
+              node_id: "UGluOjM3Mjk2MTIyNTY2MDUxNDQ=",
+              auto_alt_text: "a close up of a plate of food with meat",
+              id: "3729612256605144",
+              is_promoted: false,
+              description: "Italian Pot Roast: A Hearty and Flavorful Recipe",
+              title: "",
+              images: {
+                orig: {
+                  width: 1024,
+                  height: 1024,
+                  url: "https://i.pinimg.com/originals/c0/ad/f4/c0adf44a87470e7287219893efa5df75.webp",
+                },
+              },
+              link: "https://myauntyrecipes.com/2024/10/23/italian-pot-roast/?tp_image_id=7421",
+              domain: "myauntyrecipes.com",
+              seo_alt_text: "a close up of a plate of food with meat",
+              board: {
+                node_id: "Qm9hcmQ6MzcyOTY4MDg4MDQyNzg2OA==",
+                name: "Food",
+                cover_images: {
+                  "222x": {
+                    url: "https://i.pinimg.com/222x/fb/a4/05/fba405834dc4bb6cceefe63c7d2f566c.jpg",
+                    width: 222,
+                    height: null,
+                  },
+                },
+                owner: {
+                  node_id: "VXNlcjozNzI5NzQ5NTk5ODUyNTc1",
+                  verified_identity: {},
+                  follower_count: 85,
+                  full_name: "Courtney Elizabeth",
+                  image_large_url:
+                    "https://i.pinimg.com/140x140_RS/71/b7/c7/71b7c71b452076af02217324c26b3f08.jpg",
+                  is_ads_only_profile: false,
+                  id: "3729749599852575",
+                  image_small_url:
+                    "https://i.pinimg.com/30x30_RS/71/b7/c7/71b7c71b452076af02217324c26b3f08.jpg",
+                  image_medium_url:
+                    "https://i.pinimg.com/75x75_RS/71/b7/c7/71b7c71b452076af02217324c26b3f08.jpg",
+                  ads_only_profile_site: null,
+                  is_primary_website_verified: false,
+                  is_verified_merchant: false,
+                  username: "csadak",
+                },
+                id: "3729680880427868",
+                url: "/csadak/food/",
+                collaborating_users: [],
+                images: {
+                  "170x": [
+                    {
+                      url: "https://i.pinimg.com/170x/8c/80/93/8c80939c8ed0349262b9bb32a8e0192f.jpg",
+                      width: 170,
+                      height: 339,
+                      dominant_color: "#E1B342",
+                    },
+                  ],
+                },
+                pin_count: 423,
+                is_collaborative: false,
+                section_count: 10,
+                board_order_modified_at: "Sat, 19 Apr 2025 17:05:53 +0000",
+                type: "board",
+                collaborator_count: 0,
+              },
+              grid_title: "Savory Italian Pot Roast",
+              native_creator: null,
+              created_at: "Tue, 07 Jan 2025 18:23:09 +0000",
+              pinner: {
+                node_id: "VXNlcjozNzI5NzQ5NTk5ODUyNTc1",
+                verified_identity: {},
+                follower_count: 85,
+                full_name: "Courtney Elizabeth",
+                image_large_url:
+                  "https://i.pinimg.com/140x140_RS/71/b7/c7/71b7c71b452076af02217324c26b3f08.jpg",
+                is_ads_only_profile: false,
+                id: "3729749599852575",
+                image_small_url:
+                  "https://i.pinimg.com/30x30_RS/71/b7/c7/71b7c71b452076af02217324c26b3f08.jpg",
+                image_medium_url:
+                  "https://i.pinimg.com/75x75_RS/71/b7/c7/71b7c71b452076af02217324c26b3f08.jpg",
+                ads_only_profile_site: null,
+                is_primary_website_verified: false,
+                is_verified_merchant: false,
+                username: "csadak",
+              },
+              videos: null,
+              story_pin_data: null,
+            },
+          ],
+          cursor: "Y2JVSG81V2...",
+        },
+      },
       {
         name: "Pin",
         method: "GET",
