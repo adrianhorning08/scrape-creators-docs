f954f12fb66c376ce7893808b0007b7685bcb71e
Adrian Horning <m.adrian.horning@gmail.com>
2025-12-04 16:58:42 -0600

    use ai as fallback for tiktok transcript

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -5501,6 +5501,15 @@ export const apis = [
               "Language of the transcript. 2 letter language code, ie 'en', 'es', 'fr', 'de', 'it', 'ja', 'ko', 'zh'",
             placeholder: "en",
           },
+          {
+            name: "use_ai_as_fallback",
+            type: "string",
+            required: false,
+            description:
+              "Use AI as a fallback to get the transcript if the transcript is not found. Costs 10 credits to use this feature.",
+            placeholder: "false",
+            credits: 10,
+          },
         ],
         sampleResponse: {
           id: "7499229683859426602",
@@ -5509,26 +5518,6 @@ export const apis = [
             "WEBVTT\n\n\n00:00:00.120 --> 00:00:01.840\nAlright, pizza review time.\n\n00:00:01.841 --> 00:00:03.761\nSal's Pizza Factory. Oh,\n\n00:00:03.762 --> 00:00:05.721\nit's Fucking Corn. We're in Charlotte.\n\n00:00:05.722 --> 00:00:07.861\nAlright man, any chance I can get a real quick picture?\n\n00:00:07.920 --> 00:00:10.040\nShaq or Dwight Howard? I gotta pick one.\\n",
         },
       },
-      // {
-      //   name: "Is a TikTok Shop?",
-      //   method: "GET",
-      //   description: "Checks if a TikTok user has a shop",
-      //   path: "/v1/tiktok/user/is-shop",
-      //   params: [
-      //     {
-      //       name: "handle",
-      //       type: "string",
-      //       required: true,
-      //       description: "TikTok handle",
-      //       placeholder: "golinutrition",
-      //     },
-      //   ],
-      //   sampleResponse: {
-      //     success: true,
-      //     has_shop: true,
-      //     seller_id: "7495794203056835079",
-      //   },
-      // },
       {
         name: "TikTok Live",
         method: "GET",
