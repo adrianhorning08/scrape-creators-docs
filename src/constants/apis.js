2e08c5850df06f4f4a6650cb153be452a843d20c
Adrian Horning <m.adrian.horning@gmail.com>
2026-02-23 14:14:43 -0600

    sort_by available in facebook company ads endpoint

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -27895,6 +27895,14 @@ export const apis = [
             placeholder: "EN",
           },
           {
+            name: "sort_by",
+            type: "select",
+            required: false,
+            description: "Sort by impressions (high to low), or Most Recent (relevancy_monthly_grouped). Defaults to impressions.",
+            placeholder: "total_impressions",
+            options: ["total_impressions", "relevancy_monthly_grouped"],
+          },
+          {
             name: "start_date",
             type: "string",
             required: false,
